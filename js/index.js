213a49ba21b08c4c90f6f5c9fb734ad91cc17326
homedliu <262296804@qq.com>
2017-10-19 21:26:40 +0800

    f

---
+++ js/index.js
@@ -0,0 +1,356 @@
+$(function(){
+            var img_url='';
+            var resources = [
+                img_url+'images/img_top.png',
+                img_url+'images/img_qhg.png',
+                img_url+'images/img_qmsht.png',
+                img_url+'images/img_mqgy.png',
+                img_url+'images/img_mhg.png',
+                img_url+'images/img_gzj.png',
+                img_url+'images/logo.png',
+                img_url+'images/icon_hg.png',
+                img_url+'images/icon_hg_star.png',
+                img_url+'images/btn_right.png',
+                img_url+'images/btn_left.png',
+                img_url+'images/people_bg.png',
+                img_url+'images/logo_tx.png',
+                img_url+'images/p1_btn_wyzt.png',
+                img_url+'images/p2_title.png',
+                img_url+'images/p2_qkg.png',
+                img_url+'images/p2_piaodai.png',
+                img_url+'images/p2_text_qkg.png',
+                img_url+'images/p2_qmsht.png',
+                img_url+'images/p2_text_qmsht.png',
+                img_url+'images/p2_mqgy.png',
+                img_url+'images/p2_text_mqgy.png',
+                img_url+'images/p2_mhg.png',
+                img_url+'images/p2_text_mhg.png',
+                img_url+'images/p2_gzj.png',
+                img_url+'images/p2_text_gzj.png',
+                img_url+'images/p2_btn_ljzt.png',
+                img_url+'images/icon_tx_kuang.png',
+                img_url+'images/white_star.png',
+                img_url+'images/icon_qi.png',
+                img_url+'images/p3_pr_lrbg.png',
+                img_url+'images/icon_dz.png',
+                img_url+'images/icon_dz_time.png',
+                img_url+'images/icon_f_left.png',
+                img_url+'images/icon_f_right.png',
+                img_url+'images/pop_bg_top.png',
+                img_url+'images/pop_bg.png',
+                img_url+'images/pop_bg_bottom.png',
+                img_url+'images/icon_p_z.png',
+                img_url+'images/p2_piaodai.png',
+                img_url+'images/pop_title_yxgz.png',
+                img_url+'images/gz_text01.png',
+                img_url+'images/gz_text02.png',
+                img_url+'images/btn_playgame.png',
+                img_url+'images/pop_title_phb.png',
+                img_url+'images/icon_shangdian.png',
+                img_url+'images/btn_phb.png',
+                img_url+'images/btn_playagrin.png',
+                img_url+'images/pop_title_zxcg.png',
+                img_url+'images/icon_heart.png',
+                img_url+'images/cg_text.png',
+                img_url+'images/cj_text.png',
+                img_url+'images/cj_bg.png',
+                img_url+'images/cj_img.png',
+                img_url+'images/cj_box_bg.png',
+                img_url+'images/no_zhong.png',
+                img_url+'images/zhong.png',
+                img_url+'images/btn_scth.png'
+            ];
+            var loadCount=0;
+            function loaded(){
+                loadCount++;
+                var percent = parseInt(loadCount/resources.length*100);
+                if(resources.length==loadCount){
+                    //百分比进度
+                    $("#lo_pr_bg").css("width",percent+'%');
+                    $('.current').text(percent+'%');
+                    startView();//开始页面
+                }else{
+                    $("#lo_pr_bg").css("width",percent+'%');
+                    $('.current').text(percent+'%');
+                }
+            }
+            function loadImg(src){
+                var image = new Image();
+                image.onload = loaded;
+                image.onerror = loaded;
+                image.src = src;
+            }
+            for(var i in resources){
+                loadImg(resources[i]);
+
+            }
+
+});
+function  startView() {
+    $(".loading_page").fadeOut(300);
+   $(".index_page").show().addClass("index_animation");
+}
+$(function(){
+    $(".btn_music").bind('touchstart',function(){
+        if ($(this).hasClass('rotate')){
+            $(this).removeClass("rotate");
+            $(this).find("audio").get(0).pause();
+        }else{
+            $(this).find("audio").get(0).play();
+            $(this).addClass("rotate");
+        }
+
+    });
+    //我要追星按钮
+    $(".p1_btn_wyzt").bind('touchstart',function(){
+        $(".index_page").fadeOut(300);
+        $(".track_sel").fadeIn(300);
+    });
+    //立即追她
+    $(".btn_ljzt").bind('touchstart',function(){
+        var i=$(".flex-active-slide").index()+1;
+        $(".track_sel").fadeOut(300);
+        $(".game_page").fadeIn(300);
+        $(".game_star").fadeIn(300);
+        $(".game_bg").find("img").eq(i).fadeIn(300);
+        $(".pop_box").fadeIn(300);
+        var sceneI=$("#hd_scene");
+        switch(i)
+        {
+            case 1:
+                sceneI.html("秦王宫");
+                break;
+            case 2:
+                sceneI.html("清明上河图");
+                break;
+            case 3:
+                sceneI.html("明清宫苑");
+                break;
+            case 4:
+                sceneI.html("梦幻谷");
+                break;
+            case 5:
+                sceneI.html("广州街");
+                break;
+            default:
+                return;
+        }
+        $(".btn_music").addClass("playgaming");
+        $(".people_run").css("bottom","80px");
+
+    });
+    //开始游戏按钮
+    $(".btn_playgame").bind('touchstart',function(){
+       $(".page_gz_box,.pop_box").fadeOut(300);
+        $("#ready_box").show().addClass("ready_ani");
+        setTimeout(function(){
+            gameInit();
+        },4500);
+
+
+    });
+    var gameScene=$(".game_scene");
+    var peopleRun=$(".people_run");
+    var leftTime;//定义点击左脚当前时间值；
+    var rightTime;//定义点击右脚当前时间值；
+    var btnTime;//定义两按钮时间差；
+
+    //////****游戏变量****///////
+    var gameTime=30;//定义游戏游玩时间；
+    var meterNum=1000;//定义路程一共长度；
+    var btnNum=140;//定义全程下需按钮次数；
+    var distanceNum=300;//定义初始进度条和红旗的距离；
+    var hgGoSpeed=300;//定义红旗前进速度，越小越快；
+
+
+    var hqSeat=(distanceNum/meterNum)*100;//红旗初始位置
+    var hqSeatNow=hqSeat;//红旗当前位置
+    var btnMeterNum=Math.floor(meterNum/btnNum);//点击一次走的距离长度。注意值最好为整数；
+    var hqSeatNowItem=(btnMeterNum/meterNum)*100;//红旗自动走的样式距离。注意值最好为整数；
+    var distanceNumNow=distanceNum;//当前进度条和红旗的距离；
+    var prBgSeat=0;//进度条当前位置；
+    var gameTimeNow=gameTime;//当前游戏剩余时间；
+    $("#starman").css("left",hqSeat+"%");
+    $("#prBg").css("width",hqSeat+"%");
+function gameInit(){
+    gameplay=true;
+    $("#ready_box").fadeOut(300);
+    prBgSeat=0;
+    //$("#num_qi").css("right",hqSeat+"%");
+    $("#starman").css("left",hqSeat+"%");
+    $("#meterNow").html(distanceNum+"m");
+    $("#prBg").css("width",hqSeat+"%");
+    $("#user_tx").css("left",prBgSeat+"%");
+    hqSeatNow=hqSeat;
+
+    gameTimeNow=gameTime;
+    distanceNumNow=distanceNum;
+    hqGo=setInterval(function(){
+        if(hqSeatNow<100)
+        {
+            hqSeatNow += hqSeatNowItem;
+            distanceNumNow += btnMeterNum;
+            $("#starman").css("left",hqSeatNow+"%");
+            $("#prBg").css("width",hqSeatNow+"%");
+            //$("#num_qi").css("right", hqSeatNow + "%");
+            $("#meterNow").html(distanceNumNow + "m");
+        }else{
+            clearInterval(hqGo);
+            clearInterval(gamingTime);
+            $(".page_sb_box,.pop_box").fadeIn(300);
+            gameplay=false;
+        }
+    },hgGoSpeed);
+    gamingTime=setInterval(function(){
+        gameTimeNow--;
+        if(gameTimeNow>=0){
+            $("#game_time i").html(gameTimeNow+"秒");
+        }else{
+            clearInterval(gamingTime);
+            clearInterval(hqGo);
+            $(".page_sb_box,.pop_box").fadeIn(300);
+            $("#game_time i").html(0+"秒");
+            gameplay=false;
+        }
+    },1000)
+}
+    function gameBtn(){
+        gameScene.removeClass("paused");
+        peopleRun.removeClass("paused");
+        gameScene.addClass("game_anmation");
+        peopleRun.addClass("run_ani");
+        prBgSeat+=hqSeatNowItem;
+        distanceNumNow -= btnMeterNum;
+        $("#user_tx").css("left",prBgSeat+"%");
+        $("#meterNow").html(distanceNumNow + "m");
+        if(distanceNumNow<=0){
+            $(".page_cg_box,.pop_box").fadeIn(300);
+            clearInterval(hqGo);
+            clearInterval(gamingTime);
+            $("#meterNow").html(0 + "m");
+            $("#cg_time").html(gameTime-gameTimeNow+"S")
+        }
+    }
+
+    function paused(){
+        gameScene.addClass("paused");
+        peopleRun.addClass("paused");
+    }
+    pause=setTimeout(paused,1000);
+    clearTimeout(pause);
+    var runSel;
+    //左脚按钮
+    $(".btn_left").bind('touchend',function(){
+        if(gameplay){
+        leftTime= new Date().getTime();
+        btnTime=Math.abs(leftTime-rightTime);
+        if(btnTime<350){
+            if(gameScene.has("fast")||peopleRun.has("fast")) {
+                gameScene.addClass("game_anmation");
+                peopleRun.addClass("run_ani");
+            }
+        }
+        clearTimeout(pause);
+        if(runSel==undefined){
+            runSel=true;
+        }
+        if(runSel) {
+            runSel=false;
+            gameBtn();
+        }else{
+            gameScene.addClass("paused");
+            peopleRun.addClass("paused");
+        }
+        if (peopleRun.has("run_ani")) {
+            pause=setTimeout(paused, 1000);
+        }
+    }});
+
+    $(".btn_right").bind('touchend',function(){
+        if(gameplay){
+        rightTime= new Date().getTime();
+        btnTime=Math.abs(leftTime-rightTime);
+        if(btnTime<350){
+            if(!gameScene.has("fast")||peopleRun.has("fast")) {
+                gameScene.addClass("fast");
+                peopleRun.addClass("fast");
+            }
+        }else{
+            if(!gameScene.has("fast")||peopleRun.has("fast")) {
+                gameScene.removeClass("fast");
+                peopleRun.removeClass("fast");
+            }
+        }
+        clearTimeout(pause);
+        if(runSel==undefined){
+            runSel=false;
+        }
+        if(!runSel) {
+            runSel=true;
+            gameBtn();
+        }else{
+            gameScene.addClass("paused");
+            peopleRun.addClass("paused");
+        }
+        if (peopleRun.has("run_ani")) {
+            pause=setTimeout(paused, 1000);
+        }
+    }});
+
+    //排行榜关闭按钮
+    $(".btn_close").bind('touchstart',function(){
+        $(".page_phb_box").fadeOut(300);
+
+    });
+    //打开排行榜按钮
+    $(".btn_phb").bind('touchstart',function(){
+        $(".page_phb_box").fadeIn(300);
+        loaded();
+    });
+    //
+    $(".btn_z").bind('touchstart',function(){
+        gameplay=false;
+        $(".pop_box,.page_cg_box").fadeOut(300);
+        $("#ready_box").show().addClass("ready_ani");
+        setTimeout(function(){
+            gameInit();
+        },4500);
+    });
+    $(".btn_p").bind('touchstart',function(){
+        $(".page_phb_box").fadeIn(300);
+        //$(".page_cg_box").fadeOut(300);
+        loaded();
+    });
+    var gameplay=false;
+    $(".btn_playagrin").bind('touchstart',function(){
+        gameplay=false;
+        $(".pop_box,.page_sb_box").fadeOut(300);
+        $("#ready_box").show().addClass("ready_ani");
+        setTimeout(function(){
+            gameInit();
+        },4500);
+    });
+    $(".btn_cjyhl").bind('touchstart',function(){
+        $(".page_cg_box").fadeOut(300);
+        $(".page_cj_box").fadeIn(300);
+        $("#gj_con").wScratchPad({
+            size        : 15,
+            bg          : false,
+            fg          : "./images/cj_img.png",
+            realtime    : true,
+            //     scratchDown : null,
+            scratchDown   : function(e, percent){
+                $(".win_nowin").show();
+            },
+            scratchMove : function(e, percent){
+                if(percent >50){
+                    this.clear();
+                }
+            }
+        });
+    });
+    $(".btn_cj_close").bind('touchstart',function(){
+        $(".page_cj_box").fadeOut(300);
+        $(".page_cg_box").fadeIn(300);
+    });
+});
---
+++ js/index1.js
@@ -0,0 +1,311 @@
+$(function(){
+            var img_url='';
+            var resources = [
+                img_url+'images/img_top.png',
+                img_url+'images/img_qhg.png',
+                img_url+'images/img_qmsht.png',
+                img_url+'images/img_mqgy.png',
+                img_url+'images/img_mhg.png',
+                img_url+'images/img_gzj.png',
+                img_url+'images/logo.png',
+                img_url+'images/icon_hg.png',
+                img_url+'images/icon_hg_star.png',
+                img_url+'images/btn_right.png',
+                img_url+'images/btn_left.png',
+                img_url+'images/people_bg.png',
+                img_url+'images/logo_tx.png',
+                img_url+'images/p1_btn_wyzt.png',
+                img_url+'images/p2_title.png',
+                img_url+'images/p2_qkg.png',
+                img_url+'images/p2_piaodai.png',
+                img_url+'images/p2_text_qkg.png',
+                img_url+'images/p2_qmsht.png',
+                img_url+'images/p2_text_qmsht.png',
+                img_url+'images/p2_mqgy.png',
+                img_url+'images/p2_text_mqgy.png',
+                img_url+'images/p2_mhg.png',
+                img_url+'images/p2_text_mhg.png',
+                img_url+'images/p2_gzj.png',
+                img_url+'images/p2_text_gzj.png',
+                img_url+'images/p2_btn_ljzt.png',
+                img_url+'images/icon_tx_kuang.png',
+                img_url+'images/white_star.png',
+                img_url+'images/icon_qi.png',
+                img_url+'images/p3_pr_lrbg.png',
+                img_url+'images/icon_dz.png',
+                img_url+'images/icon_dz_time.png',
+                img_url+'images/icon_f_left.png',
+                img_url+'images/icon_f_right.png',
+                img_url+'images/pop_bg_top.png',
+                img_url+'images/pop_bg.png',
+                img_url+'images/pop_bg_bottom.png',
+                img_url+'images/p2_piaodai.png',
+                img_url+'images/pop_title_yxgz.png',
+                img_url+'images/gz_text01.png',
+                img_url+'images/gz_text02.png',
+                img_url+'images/btn_playgame.png',
+                img_url+'images/pop_title_phb.png',
+                img_url+'images/icon_shangdian.png',
+                img_url+'images/btn_phb.png',
+                img_url+'images/btn_playagrin.png',
+                img_url+'images/pop_title_zxcg.png',
+                img_url+'images/icon_heart.png',
+                img_url+'images/cg_text.png',
+                img_url+'images/btn_fxhy.png'
+            ];
+            var loadCount=0;
+            function loaded(){
+                loadCount++;
+                var percent = parseInt(loadCount/resources.length*100);
+                if(resources.length==loadCount){
+                    //百分比进度
+                    $("#lo_pr_bg").css("width",percent+'%');
+                    $('.current').text(percent+'%');
+                    startView();//开始页面
+                }else{
+                    $("#lo_pr_bg").css("width",percent+'%');
+                    $('.current').text(percent+'%');
+                }
+            }
+            function loadImg(src){
+                var image = new Image();
+                image.onload = loaded;
+                image.onerror = loaded;
+                image.src = src;
+            }
+            for(var i in resources){
+                loadImg(resources[i]);
+
+            }
+
+});
+function  startView() {
+    $(".loading_page").fadeOut(300);
+    //$(".index_page").show().addClass("index_animation");
+}
+$(function(){
+    $(".btn_music").bind('touchstart',function(){
+        if ($(this).hasClass('rotate')){
+            $(this).removeClass("rotate");
+            $(this).find("audio").get(0).pause();
+        }else{
+            $(this).find("audio").get(0).play();
+            $(this).addClass("rotate");
+        }
+
+    });
+    //我要追星按钮
+    $(".p1_btn_wyzt").bind('touchstart',function(){
+        $(".index_page").fadeOut(300);
+        $(".track_sel").fadeIn(300);
+    });
+    //立即追她
+    $(".btn_ljzt").bind('touchstart',function(){
+        var i=$(".flex-active-slide").index()+1;
+        $(".track_sel").fadeOut(300);
+        $(".game_page").fadeIn(300);
+        $(".game_star").fadeIn(300);
+        $(".game_bg").find("img").eq(i).fadeIn(300);
+        $(".pop_box").fadeIn(300);
+        var sceneI=$("#hd_scene");
+        switch(i)
+        {
+            case 1:
+                sceneI.html("秦王宫");
+                break;
+            case 2:
+                sceneI.html("清明上河图");
+                break;
+            case 3:
+                sceneI.html("明清宫苑");
+                break;
+            case 4:
+                sceneI.html("梦幻谷");
+                break;
+            case 5:
+                sceneI.html("广州街");
+                break;
+            default:
+                return;
+        }
+        $(".btn_music").addClass("playgaming");
+        $(".people_run").css("bottom","80px");
+
+    });
+    //开始游戏按钮
+    $(".btn_playgame").bind('touchstart',function(){
+       $(".page_gz_box,.pop_box").fadeOut(300);
+        gameInit();
+
+    });
+
+    var gameTime=30;//定义游戏时间；
+    var meterNum=1000;//定义游戏米数；
+
+
+    var leftTime;//定义点击左脚当前时间值；
+    var rightTime;//定义点击右脚当前时间值；
+    var btnTime;//定义两按钮时间差；
+
+    var gameTimeNow=gameTime;//定义游戏时间；
+    var btnNum=100;//定义游戏按钮次数；
+    var btnNumNow=0;//定义已按游戏按钮次数；
+
+    var meterNumNow=0;//定义已走米数；
+    var prBgNow=0;//定义进度条；
+    var disparityNum=500;//定义用户和明星初始距离米数；
+    var disparityNumNow=disparityNum;//定义用户和明星当前距离米数；
+    var prBg=(disparityNum/meterNum)*100;//定义用初始进度条宽度；
+    var btnNumMete=Math.ceil(meterNum/btnNum);//定义点击一次走的米数；
+    var gameScene=$(".game_scene");
+    var peopleRun=$(".people_run");
+    //游戏开始初始化
+
+    var numqiRight=((meterNum-disparityNum)/meterNum)*100;//初始红旗的位置；
+    var numqiSpeedNum=gameTime*1000/300;//初始红旗明星的速度次数；
+    var numqiSpeedMeterNum=numqiRight/numqiSpeedNum;//初始红旗明星的米数速度次数；
+    var numqiSpeedNumNow=0;//初始红旗明星的当前速度次数；
+    var numqiGoNum=(meterNum-disparityNum)/numqiSpeedNum;//红旗前进的实际米数；
+    function gameInit(){
+        //btnNumNow=0;
+        gameTimeNow=gameTime;
+        disparityNumNow=disparityNum;
+        $("#meterNow").html(disparityNum+"m");
+        //$("#prBg").css("width","1%");
+        timeNow=setInterval(function(){
+            gameTimeNow--;
+            if(gameTimeNow>=0){
+                $("#game_time i").html(gameTimeNow+"秒");
+            }else{
+                clearInterval(timeNow);
+                $(".page_sb_box,.pop_box").fadeIn(300);
+                $("#game_time i").html(0+"秒");
+            }
+        },1000);
+
+
+        $("#num_qi").css("right",numqiRight+"%");
+        numqitimeNow=setInterval(function(){
+            if(numqiSpeedNumNow<numqiSpeedNum){
+                numqiSpeedNumNow++;
+                disparityNumNow+=numqiGoNum;
+                numqiRight-=numqiSpeedMeterNum;
+                if(disparityNumNow>meterNum){
+                    disparityNumNow=parseInt(disparityNumNow);
+                }
+            $("#num_qi").css("right",numqiRight+"%");
+                $("#meterNow").html(disparityNumNow+"m");
+
+            }else{
+                clearInterval(numqitimeNow);
+            }
+        },300);
+    }
+    var prBgWidth=$("#prBg").width();//进度条当前宽度；
+    function gameBtn(){
+        gameScene.removeClass("paused");
+        peopleRun.removeClass("paused");
+        gameScene.addClass("game_anmation");
+        peopleRun.addClass("run_ani");
+
+
+
+        prBgWidth+=numqiSpeedMeterNum;
+        $("#prBg").css("width",prBgWidth+"%");
+        disparityNumNow-=numqiGoNum;
+        $("#meterNow").html(disparityNumNow+"m");
+        if(100-prBgWidth<numqiRight){
+                $(".page_cg_box,.pop_box").fadeIn(300);
+                $("#cg_time").html(gameTime-gameTimeNow+"S");
+            }
+        }
+    gameInit();
+    function paused(){
+        gameScene.addClass("paused");
+        peopleRun.addClass("paused");
+    }
+    pause=setTimeout(paused,1000);
+    clearTimeout(pause);
+    var runSel;
+    //左脚按钮
+    $(".btn_left").bind('touchstart',function(){
+        leftTime= new Date().getTime();
+        btnTime=Math.abs(leftTime-rightTime);
+        if(btnTime<350){
+            if(gameScene.has("fast")||peopleRun.has("fast")) {
+                gameScene.addClass("game_anmation");
+                peopleRun.addClass("run_ani");
+            }
+        }
+        clearTimeout(pause);
+        if(runSel==undefined){
+            runSel=true;
+        }
+        if(runSel) {
+            runSel=false;
+            gameBtn();
+        }else{
+            gameScene.addClass("paused");
+            peopleRun.addClass("paused");
+        }
+        if (peopleRun.has("run_ani")) {
+            pause=setTimeout(paused, 1000);
+        }
+    });
+
+    $(".btn_right").bind('touchstart',function(){
+        rightTime= new Date().getTime();
+        btnTime=Math.abs(leftTime-rightTime);
+        if(btnTime<350){
+            if(!gameScene.has("fast")||peopleRun.has("fast")) {
+                gameScene.addClass("fast");
+                peopleRun.addClass("fast");
+            }
+        }else{
+            if(!gameScene.has("fast")||peopleRun.has("fast")) {
+                gameScene.removeClass("fast");
+                peopleRun.removeClass("fast");
+            }
+        }
+        clearTimeout(pause);
+        if(runSel==undefined){
+            runSel=false;
+        }
+        if(!runSel) {
+            runSel=true;
+            gameBtn();
+        }else{
+            gameScene.addClass("paused");
+            peopleRun.addClass("paused");
+        }
+        if (peopleRun.has("run_ani")) {
+            pause=setTimeout(paused, 1000);
+        }
+    });
+
+    //排行榜关闭按钮
+    $(".btn_close").bind('touchstart',function(){
+        $(".pop_box,.page_phb_box").fadeOut(300);
+
+    });
+    //打开排行榜按钮
+    $(".btn_phb").bind('touchstart',function(){
+        $(".page_phb_box").fadeIn(300);
+        $(".page_sb_box").fadeOut(300);
+        loaded();
+    });
+    //
+    $(".btn_z").bind('touchstart',function(){
+        $(".pop_box,.page_phb_box").fadeOut(300);
+        gameInit();
+    });
+    $(".btn_p").bind('touchstart',function(){
+        $(".page_phb_box").fadeIn(300);
+        $(".page_cg_box").fadeOut(300);
+        loaded();
+    });
+    $(".btn_playagrin").bind('touchstart',function(){
+        $(".pop_box,.page_sb_box").fadeOut(300);
+        gameInit();
+    });
+});
